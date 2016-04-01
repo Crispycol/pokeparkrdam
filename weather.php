17af1b9c156aa42bebcb43812a29d9777ff760e8
Wizzr0 <cp.boer97@gmail.com>
2016-04-01 14:57:14 +0200

    Update weather.php

--- weather.php
+++ weather.php
@@ -11,7 +11,7 @@
 <link rel="stylesheet" href="style.css"/>
 <body>
 
-<h1><img src="logo.png" style="width:450px;height:228px;"></h1>
+<h1><img src="logo.png" style="width:350px;height:auto;"></h1>
 
 <div class="container">
     <header>
@@ -34,59 +34,74 @@
 
     <div class="first">
         <div class="calm">
-        <h3>Calm</h3>
+        <h3><img src="calm.png" style="width:40px;height:auto;">Calm</h3>
         <p>
-            The weather is calm and peaceful, a lot of Pok&eacute;mon will around this time!<br>
+            The weather is calm and peaceful, a lot of Pok&eacute;mon will be around this time!<br>
             Appearing Types:<br>
             <img src="normaltype.png" style="width:70px;height:auto;">
             <img src="grasstype.png" style="width:70px;height:auto;">
+            <img src="bugtype.png" style="width:70px;height:auto;">
+            <img src="groundtype.png" style="width:70px;height:auto;">
+            <img src="poisontype.png" style="width:70px;height:auto;">
         </p>
         </div>
 
         <div class="sunny">
-        <h3>Sunny</h3>
+        <h3><img src="sunny.png" style="width:40px;height:auto;">Sunny</h3>
         <p>
             It is hot outside which means Fire Pok&eacute;mon are more common to appear!<br>
             Appearing Types:<br>
             <img src="firetype.png" style="width:70px;height:auto;">
+            <img src="grasstype.png" style="width:70px;height:auto;">
+            <img src="fightingtype.png" style="width:70px;height:auto;">
+            <img src="fairytype.png" style="width:70px;height:auto;">
         </p>
         </div>
 
         <div class="rainy">
-        <h3>Rainy</h3>
+        <h3><img src="rainy.png" style="width:40px;height:auto;">Rainy</h3>
         <p>
             Once its raining you might want to get wet as the Water Pok&eacute;mon are very active during this time!<br>
             Appearing Types:<br>
             <img src="watertype.png" style="width:70px;height:auto;">
+            <img src="rocktype.png" style="width:70px;height:auto;">
+            <img src="psychictype.png" style="width:70px;height:auto;">
         </p>
         </div>
     </div>
 
     <div class="second">
         <div class="windy">
-            <h3>Windy</h3>
+            <h3><img src="windy.png" style="width:40px;height:auto;">Windy</h3>
             <p>
                 Once it is windy outside Flying type Pok&eacute;mon like to soar through the air as well as other types like Bug!<br>
                 Appearing Types:<br>
                 <img src="flyingtype.png" style="width:70px;height:auto;">
+                <img src="psychictype.png" style="width:70px;height:auto;">
+                <img src="ghosttype.png" style="width:70px;height:auto;">
+                <img src="fairytype.png" style="width:70px;height:auto;">
             </p>
         </div>
 
         <div class="stormy">
-            <h3>Stormy</h3>
+            <h3><img src="stormy.png" style="width:40px;height:auto;">Stormy</h3>
             <p>
                 In this dreadful weather it might be cool to go outside to catch some Dragon or Dark Pok&eacute;mon!<br>
                 Appearing Types:<br>
                 <img src="electrictype.png" style="width:70px;height:auto;">
+                <img src="darktype.png" style="width:70px;height:auto;">
+                <img src="rocktype.png" style="width:70px;height:auto;">
+                <img src="dragontype.png" style="width:70px;height:auto;">
             </p>
         </div>
 
         <div class="icy">
-            <h3>Icy</h3>
+            <h3><img src="icy.png" style="width:40px;height:auto;">Icy</h3>
             <p>
                 Once the cold begins to settle try to go outside and catch some nice Ice Pok&eacute;mon.<br>
                 Appearing Types:<br>
                 <img src="icetype.png" style="width:70px;height:auto;">
+                <img src="steeltype.png" style="width:70px;height:auto;">
             </p>
         </div>
     </div>
