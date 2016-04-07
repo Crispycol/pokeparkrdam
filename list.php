7ec9ebfbec0b55de0287fedfc749f5b9c41cb780
Wizzr0 <cp.boer97@gmail.com>
2016-04-07 16:33:08 +0200

    Create list.php

---
+++ list.php
@@ -0,0 +1,104 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pok&eacute;Park Rotterdam</title>
+</head>
+<link rel="stylesheet" href="style.css"/>
+<body>
+
+<h1><img src="logo.png" style="width:450px;height:228px;"></h1>
+
+<div class="container">
+    <header>
+        <nav>
+            <ul>
+                <li><a href=index.php>Home</a></li>
+                <li><a href=gameplay.php>Gameplay</a></li>
+                <li><a href=list.php class="active">List</a></li>
+                <li><a href=weather.php>Weather</a></li>
+            </ul>
+        </nav>
+    </header>
+
+<main>
+    <p>On this page you can look which Pok&eacutemon can be caught in Pok&eacutePark Rotterdam. Their image, name and type is displayed in the table below. To find out when you can find a certain Pok&eacutemon you can look at their typing and see in the <a href=weather.php>Weather</a> page with which weather they will appear.</p>
+    <table style="width:100%">
+        <tr>
+            <th>Number</th>
+            <th>Pok&eacute;mon</th>
+            <th>Typing</th>
+        </tr>
+        <tr>
+            <td>#001</td>
+            <td><img src="bulbasaur.png" width="100px" height="auto"><br>Bulbasaur</td>
+            <td><img src="grasstype.png" style="width:70px;height:auto;"><img src="poisontype.png" style="width:70px;height:auto;"></td>
+        </tr>
+        <tr>
+            <td>#002</td>
+            <td><img src="ivysaur.png" width="100px" height="auto"><br>Ivysaur</td>
+            <td><img src="grasstype.png" style="width:70px;height:auto;"><img src="poisontype.png" style="width:70px;height:auto;"></td>
+        </tr>
+        <tr>
+            <td>#003</td>
+            <td><img src="venusaur.png" width="100px" height="auto"><br>Venusaur</td>
+            <td><img src="grasstype.png" style="width:70px;height:auto;"><img src="poisontype.png" style="width:70px;height:auto;"></td>
+        </tr>
+        <tr>
+            <td>#004</td>
+            <td><img src="charmander.png" width="100px" height="auto"><br>Charmander</td>
+            <td><img src="firetype.png" style="width:70px;height:auto;"></td>
+        </tr>
+        <tr>
+            <td>#005</td>
+            <td><img src="charmeleon.png" width="100px" height="auto"><br>Charmeleon</td>
+            <td><img src="firetype.png" style="width:70px;height:auto;"></td>
+        </tr>
+        <tr>
+            <td>#006</td>
+            <td><img src="charizard.png" width="100px" height="auto"><br>Charizard</td>
+            <td><img src="firetype.png" style="width:70px;height:auto;"><img src="flyingtype.png" style="width:70px;height:auto;"></td>
+        </tr>
+        <tr>
+            <td>#007</td>
+            <td><img src="squirtle.png" width="100px" height="auto"><br>Squirtle</td>
+            <td><img src="watertype.png" style="width:70px;height:auto;"></td>
+        </tr>
+        <tr>
+            <td>#008</td>
+            <td><img src="wartortle.png" width="100px" height="auto"><br>Wartortle</td>
+            <td><img src="watertype.png" style="width:70px;height:auto;"></td>
+        </tr>
+        <tr>
+            <td>#009</td>
+            <td><img src="blastoise.png" width="100px" height="auto"><br>Blastoise</td>
+            <td><img src="watertype.png" style="width:70px;height:auto;"></td>
+        </tr>
+        <tr>
+            <td>#025</td>
+            <td><img src="pikachu.png" width="100px" height="auto"><br>Pikachu</td>
+            <td><img src="electrictype.png" style="width:70px;height:auto;"></td>
+        </tr>
+        <tr>
+            <td>#026</td>
+            <td><img src="raichu.png" width="100px" height="auto"><br>Raichu</td>
+            <td><img src="electrictype.png" style="width:70px;height:auto;"></td>
+        </tr>
+        <tr>
+            <td>#150</td>
+            <td><img src="mewtwo.png" width="100px" height="auto"><br>Mewtwo</td>
+            <td><img src="psychictype.png" style="width:70px;height:auto;"></td>
+        </tr>
+        <tr>
+            <td>#151</td>
+            <td><img src="mew.png" width="100px" height="auto"><br>Mew</td>
+            <td><img src="psychictype.png" style="width:70px;height:auto;"></td>
+        </tr>
+    </table>
+</main>
+</body>
+</html>
